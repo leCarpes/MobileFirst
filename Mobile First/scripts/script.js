b358ee5a60ab86d6b200be3e4c140e766c46dd94
Letícia Carpes <lecarpesoliveira@gmail.com>
2024-05-18 10:53:12 -0300

    responsividade

--- Mobile First/scripts/script.js
+++ Mobile First/scripts/script.js
@@ -4,24 +4,27 @@ const headerLinks2 = document.querySelector(".header-style__links2");
 
 function toggleModal1() {
     if (headerLinks.classList.contains("open")) {
-        headerLinks.classList.remove("open")
+        headerLinks.classList.remove("open");
     }else{
-        headerLinks.classList.add("open")
-        headerLinks2.classList.remove("open2")
-    }
-    
+        headerLinks.classList.add("open");
+        headerLinks2.classList.remove("open2");
+    } 
 }
+
+
 
 
 function toggleModal2() {
     if (headerLinks2.classList.contains("open2")) {
-        headerLinks2.classList.remove("open2")
+        headerLinks2.classList.remove("open2");
     }else{
-        headerLinks2.classList.add("open2")
-        headerLinks.classList.remove("open")
+        headerLinks2.classList.add("open2");
+        headerLinks.classList.remove("open");
     }
     
 }
+
+
 
 
 document.getElementById('handleNavbar').addEventListener('click', toggleModal1)
@@ -39,16 +42,13 @@ function setTheme(theme) {
 
 function getTheme() {
     if (localStorage.getItem("theme") === "theme-dark") {
-        document
-        .getElementById("toggle")
-        .classList.replace("bi-brightness-high-fill", "bi-moon-fill")
+
+        document.getElementById("toggle").classList.replace("bi-brightness-high-fill", "bi-moon-fill");
 
         setTheme("theme-light");
     } else{
 
-        document
-        .getElementById("toggle")
-        .classList.replace("bi-moon-fill", "bi-brightness-high-fill")
+        document.getElementById("toggle").classList.replace("bi-moon-fill", "bi-brightness-high-fill");
 
         setTheme("theme-dark");
     }
@@ -56,13 +56,13 @@ function getTheme() {
 
 (function () {
     if(localStorage.getItem("theme") === "theme-dark") {
-        setTheme("theme-dark")
+        setTheme("theme-dark");
     } else {
-        setTheme("theme-light")
+        setTheme("theme-light");
     }
 
-    headerLinks.classList.remove("open")
-    headerLinks2.classList.remove("open2")
+    headerLinks.classList.remove("open");
+    headerLinks2.classList.remove("open2");
 
 })();
     
